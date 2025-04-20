4520907f445a908c5858345b9ad0d601fc24ac9a
Aleksei Leskin <aleksei_988@mail.ru>
2025-04-20 16:08:40 +0300

    add sprint4 homework

---
+++ src/SubTask.java
@@ -0,0 +1,23 @@
+public class SubTask extends Task {
+    private int epicId;
+
+    public SubTask(String name, String description, int id, Status status, int epicId) {
+        super(name, description, id, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "SubTask{" +
+                "epicId=" + epicId +
+                "} " + super.toString();
+    }
+}
